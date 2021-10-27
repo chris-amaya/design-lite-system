e49fe4bd60d8a3cba2e97f9cdb510a0f99d972d8
Chris Amaya <amayarivasc@gmail.com>
2021-10-27 16:32:12 -0500

    ADD: basic button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import styled from 'styled-components'
+
+const PrimaryButton = styled.button`
+  background-color: #0070f3;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  font-size: 1em;
+`
+
+export {PrimaryButton}
